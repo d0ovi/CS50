34b1fe27504d0a90479dcb729c9830d73b2ec5a4
Dovile Krekenaite <35746175+d0ovi@users.noreply.github.com>
2021-03-06 15:59:21 +0200

    Update mario.c

--- pset1/mario less/mario.c
+++ pset1/mario less/mario.c
@@ -3,33 +3,30 @@
 
 int main(void)
 {
-    int number, space, hash;
-
-    //get input from user
+    int size;
+    //first, let's ask user for the piramid number
     do
     {
-        number = get_int("Height : ");
+        size = get_int("Height: ");
     }
-    while (number <= 0 || number > 8);
-    
-    for (int i = 0; i < number; i++)
-    {
-        //counting spaces & ## for all lines
-        space = number - (i + 1);
-        hash = number - space;
+    //the number should be less than 8 and more than 1 (included both numbers)
+    while (size > 8 || size < 1);
 
-        //printing spaces
-        for (int s = 0; s < space; s++)
+    // in the loops we need to count spaces - 1 from the piramid size
+    //and hashes from 1 to size in next lines
+    for (int i = 0; i < size; i++)
+    {
+        for (int s = 0; s < size - (i + 1); s++)
         {
             printf(" ");
         }
 
-        //printing # piramid
-        for (int h = 0; h < hash; h++)
+        for (int j = 0; j < i + 1; j++)
         {
-            printf("#");
-        }
 
+            printf("#");
+
+        }
         printf("\n");
     }
 }
